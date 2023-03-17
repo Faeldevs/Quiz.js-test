91a448df86600c908e70a95fa90243426bfc2cbc
Rafael S. Silva <103973828+Faeldevs@users.noreply.github.com>
2023-03-17 17:05:31 -0300

    Inserindo arquivos do projeto de estudo

---
+++ questions.js
@@ -0,0 +1,47 @@
+export default [
+    {
+      question: "Quem canta a musica Caneta Azul ",
+      answers: [
+        { option: "Mamae Falei", correct: false },
+        { option: "Poeta", correct: false },
+        { option: "Fael", correct: false },
+        { option: "Manoel Gomes", correct: true },
+      ],
+    },
+    {
+      question: "Quem é o nordestino do grupo?",
+      answers: [
+        { option: "Agnes", correct: false },
+        { option: "Talles", correct: true },
+        { option: "Ester", correct: false },
+        { option: "Fael", correct: false },
+      ],
+    },
+    {
+      question: "Qual animal a Agnes adotaria?",
+      answers: [
+        { option: "Dinossauro", correct: false },
+        { option: "Girafa", correct: false },
+        { option: "Macaco", correct: true },
+        { option: "Ester", correct: false },
+      ],
+    },
+    {
+      question: "Quem come polvorá no almoço?",
+      answers: [
+        { option: "Ester", correct: true },
+        { option: "Russo", correct: false },
+        { option: "Nazista", correct: false },
+        { option: "Bluezao", correct: false },
+      ],
+    },
+    {
+        question: "Quem é o mineiro que fala menos embolado",
+        answers: [
+          { option: "Fael", correct: true },
+          { option: "Agnes", correct: false },
+          { option: "Nicolas faz o L", correct: false },
+          { option: "Gaguinho", correct: false },
+        ],
+      },
+  ];
